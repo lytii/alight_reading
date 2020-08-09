b026f5d1694475ce2ea55867bb1b0cfc6762d5ab
Lytii <you@example.com>
2020-08-09 01:34:37 -0700

    Change cover to full image with scrolling synposis

--- app/src/main/java/com/exd/myapplication/ui/chapterlist/ChapterListFragment.kt
+++ app/src/main/java/com/exd/myapplication/ui/chapterlist/ChapterListFragment.kt
@@ -14,8 +14,10 @@ import com.exd.myapplication.R
 import com.exd.myapplication.dagger.ActivityComponent
 import com.exd.myapplication.models.Chapter
 import dagger.Component
+import io.reactivex.Observable
 import kotlinx.android.synthetic.main.activity_chapter_list.view.*
 import kotlinx.android.synthetic.main.item_chapter.view.*
+import java.util.concurrent.TimeUnit
 
 class ChapterListFragment : Fragment() {
 
@@ -30,10 +32,28 @@ class ChapterListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         view.chapter_list.adapter = adapter
+        val wait = 80/2
+        var scroll = -wait
+        // disables user touch scrolling on scrollview
+        view.book_summary_scroll.setOnTouchListener { v, event -> true }
+        Observable.interval(50, TimeUnit.MILLISECONDS)
+            .subscribe {
+                with(view.book_summary_scroll) {
+                    when {
+                        scroll < 0 -> scroll++
+                        canScrollVertically(1) -> smoothScrollBy(0, 1)
+                        scroll < wait -> scroll++
+                        else -> {
+                            smoothScrollTo(0, 0)
+                            scroll = -wait
+                        }
+                    }
+                }
+            }
 
         (activity as AppCompatActivity).run { setSupportActionBar(view.toolbar) }
         view.cover_image.setImageDrawable(
-            resources.getDrawable(R.drawable.vm_novel_image_top_half, null)
+            resources.getDrawable(R.drawable.vm_novel_image, null)
         )
         val title = getString(R.string.vending_machine_title)
         view.collapsing_toolbar.title = title
